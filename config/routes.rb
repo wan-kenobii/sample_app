cfd4dd0fa9cd202c44f6a628f6a4c17340adf7c8
wan-kenobii <wan-kenobii@yahoo.de>
2014-03-19 02:47:13 +0100

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 SampleApp::Application.routes.draw do
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
+  
   root  'static_pages#home'
+
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  
+
   #get "static_pages/home"
   #get "static_pages/help"
   #get "static_pages/about"
