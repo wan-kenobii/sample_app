b1abda99d502407dc589c2f58e22defb84edc61c
wan-kenobii <wan-kenobii@yahoo.de>
2014-03-19 21:55:33 +0100

    Add user following

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,7 +4,8 @@ class MicropostsController < ApplicationController
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
-    if @micropost.save
+
+     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
